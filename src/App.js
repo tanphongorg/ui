6167ae1d1b0ee68714def6dc6ca46fbd388409b8
Truong Dung <dtruong@ringblaze.com>
2017-06-14 18:24:18 +0700

    Add semantic UI

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import 'semantic-ui-css/semantic.min.css';
+
+import ButtonComponent from './components/ButtonExampleEmphasis'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <ButtonComponent />
         </p>
       </div>
     );
